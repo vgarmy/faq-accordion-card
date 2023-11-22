94e9809271c27732f6b34cd1a76232aad7000b8d
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2023-11-22 13:10:16 +0100

    alt

--- src/containers/card/Card.jsx
+++ src/containers/card/Card.jsx
@@ -8,8 +8,8 @@ const Card = () => {
   return (
     <div className='card_container' role="main">
         <div className='card_container_image'>
-          <img src={box} className="imageBox"alt="box" />
-          <img src={women} className="womenBox"alt="box" />
+          <img src={box} className="imageBox" alt="box" />
+          <img src={women} className="womenBox" alt="box" />
         </div>
         <div className='card_container_accordian'>
           <h1>FAQ</h1>
